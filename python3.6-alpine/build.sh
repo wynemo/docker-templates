45896e0baa8dc439a9a0a02224d90254458c1f26
Nemo.Zhang <ggdabin@gmail.com>
2020-04-14 19:32:57 +0800

    Update build.sh

--- python3.6-alpine/build.sh
+++ python3.6-alpine/build.sh
@@ -1,8 +1,8 @@
-mkdir -p ./sonarx/pip_cache
-touch ./sonarx/pip_cache/${version}
+mkdir -p ./pip_cache
+touch ./pip_cache/${version}
 echo "docker build is using proxy ${http_proxy} ${https_proxy}"
-sudo docker build --build-arg http_proxy=${http_proxy} --build-arg https_proxy=${https_proxy} -f ./sonarx/docker/Dockerfile ./sonarx/ -t  $MAINIMAGE
-sudo docker build --build-arg http_proxy=${http_proxy} --build-arg https_proxy=${https_proxy} -f ./sonarx/docker/opencv/Dockerfile ./sonarx/ -t sonar_watermark
+sudo docker build --build-arg http_proxy=${http_proxy} --build-arg https_proxy=${https_proxy} -f ./docker/Dockerfile ./ -t  $MAINIMAGE
+sudo docker build --build-arg http_proxy=${http_proxy} --build-arg https_proxy=${https_proxy} -f ./docker/opencv/Dockerfile ./ -t watermark
 echo "PWD is $PWD"
-sudo docker run --rm --name pip_copyer -v $PWD/sonarx/pip_cache:/tmp/pip_cache $MAINIMAGE sh -c 'cp /pip_cache/* /tmp/pip_cache/'
-sudo docker run --rm --name pip_copyer -v $PWD/sonarx/pip_cache:/tmp/pip_cache sonar_watermark sh -c 'cp /pip_cache/* /tmp/pip_cache/'
+sudo docker run --rm --name pip_copyer -v $PWD/pip_cache:/tmp/pip_cache $MAINIMAGE sh -c 'cp /pip_cache/* /tmp/pip_cache/'
+sudo docker run --rm --name pip_copyer -v $PWD/pip_cache:/tmp/pip_cache watermark sh -c 'cp /pip_cache/* /tmp/pip_cache/'
